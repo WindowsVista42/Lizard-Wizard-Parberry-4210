e433907fa554a215a78a715c4bbba8b3ba42f13d
Sean Moulton <34073738+WindowsVista42@users.noreply.github.com>
2021-11-03 13:23:34 -0500

    Update compile.bat to build.bat

--- Lizard-Wizard/Tools/build.bat
+++ Lizard-Wizard/Tools/build.bat
@@ -3,3 +3,9 @@ pushd "..\Media\Models"
 for /r %%v in (*.obj) do "..\..\Tools\meshconvert.exe" %%v -vbo -c -y
 
 popd
+
+pushd "..\Media\Textures"
+
+..\..\Tools\texconv.exe -r *.png -ft dds -y
+
+popd
